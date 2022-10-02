be4bbd83010cccf2b684f7c226479af133fe9a62
SungHyun Ju <wntjdgus02@pusan.ac.kr>
2022-10-03 01:24:14 +0900

    Create PROLOG.py

---
+++ PROLOG.py
@@ -0,0 +1,13 @@
+!sudo apt install swi-prolog
+!pip install pyswip
+from pyswip import Prolog
+prolog=Prolog()
+prolog.assertz("father(hyerim, taewoong)")
+prolog.assertz("father(hyerim, taeyoung)")
+bool(list(prolog.query("father(hyerim, taewoong)")))
+bool(list(prolog.query("father(hyerim, taeyeon)")))
+for per in prolog.query("father(hyerim, X)"):
+  print(per["X"])
+from google.colab import drive
+#drive.mount('/content/drive') ..?
+# PROLOG로 DFS코드 짜기
