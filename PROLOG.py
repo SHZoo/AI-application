a4b4f18e18f3866f582341275ba86cafbbb7ec0e
SungHyun Ju <wntjdgus02@pusan.ac.kr>
2022-10-03 01:34:19 +0900

    Update PROLOG.py

--- PROLOG.py
+++ PROLOG.py
@@ -9,5 +9,5 @@ bool(list(prolog.query("father(hyerim, taeyeon)")))
 for per in prolog.query("father(hyerim, X)"):
   print(per["X"])
 from google.colab import drive
-#drive.mount('/content/drive') ..?
+#drive.mount('/content/drive') 
 # PROLOG로 DFS코드 짜기
